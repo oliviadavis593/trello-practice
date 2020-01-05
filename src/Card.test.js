40503a7de56ccdd578e64eac4dbba94b08271408
Olivia Davis <olivia.davis593@gmail.com>
2020-01-04 22:43:59 -0600

    added testing files

---
+++ src/Card.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+
+describe('My Card', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div')
+        ReactDOM.render(<Card />, div)
+    
+        ReactDOM.unmountComponentAtNode(div);
+    });
+})

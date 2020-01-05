a0351618a86523ebac9f9f0eccfccb0710d42433
Olivia Davis <olivia.davis593@gmail.com>
2020-01-05 14:46:38 -0600

    created successful snapshot Card comp

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import Card from './Card';
 
 describe('Card Component', () => {
@@ -7,5 +8,12 @@ describe('Card Component', () => {
         const div = document.createElement('div')
         ReactDOM.render(<Card />, div)
         ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('render the UI as expected', () => {
+        const tree = renderer 
+            .create(<Card />)
+            .toJSON();
+            expect(tree).toMatchSnapshot();
     });
 })

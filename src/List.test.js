0bb72f9d86f1d0d211fd81949336c68d979e42b2
Olivia Davis <olivia.davis593@gmail.com>
2020-01-05 14:42:27 -0600

    create successful snapshot test

--- src/List.test.js
+++ src/List.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import List from './List';
+import { exportAllDeclaration } from '@babel/types';
 
 describe('List Component', () => {
     it('renders without crashing', () => {
@@ -13,5 +15,12 @@ describe('List Component', () => {
         const div = document.createElement('div')
         ReactDOM.render(<List />, div)
         ReactDOM.unmountComponentAtNode(div)
+    });
+
+    it('renders the UI as expected 1' , () => {
+        const tree = renderer 
+            .create(<List />)
+            .toJSON();
+            expect(tree).toMatchSnapshot();
     });
 })
